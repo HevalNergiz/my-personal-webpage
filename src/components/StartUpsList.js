d925ec171d82a80d4041a3d7815b025cedf2e50f
Heval Nergiz <1hevalnergiz1@gmail.com>
2023-07-23 18:19:11 +0300

    GDSC DEÜ added

--- src/components/StartUpsList.js
+++ src/components/StartUpsList.js
@@ -20,6 +20,21 @@ const StartUpsList = () => {
             <span className="badge bg-light">Lead Designer & Web Dev.</span>
           </a>
         </li>
+        <li>
+          <a
+            className="text-decoration-none gap-3 text-muted d-inline-flex w-100 align-items-center justify-content-start"
+            href="https://gdsc.community.dev/dokuz-eylul-university/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="">
+              <span className="border-bottom border-dark">GDSC - DEÜ</span>
+
+              <IconArrowUpRight className="ms-2" size={16}></IconArrowUpRight>
+            </span>
+            <span className="badge bg-light">Project Coordinator</span>
+          </a>
+        </li>
       </ul>
     </div>
   );
