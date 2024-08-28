fd9ccb377ad795c4371d97b7de39062901b76055
Heval Nergiz <43642789+HevalNergiz@users.noreply.github.com>
2024-08-28 19:13:14 +0300

    Update StartUpsList.js

--- src/components/StartUpsList.js
+++ src/components/StartUpsList.js
@@ -5,21 +5,7 @@ const StartUpsList = () => {
     <div>
       <h3 className="mb-4 h5">I'm part of the...</h3>
       <ul className="mb-0 fw-light d-grid gap-3 partof-list ps-0 ps-md-4">
-        <li>
-          <a
-            className="text-decoration-none gap-3 text-muted d-inline-flex w-100 align-items-center justify-content-start"
-            href="https://goaction.tech"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span className="">
-              <span className="border-bottom border-dark">GoAction Labs</span>
-
-              <IconArrowUpRight className="ms-2" size={16}></IconArrowUpRight>
-            </span>
-            <span className="badge bg-light">Lead Designer & Web Dev.</span>
-          </a>
-        </li>
+      
         {/* <li>
           <a
             className="text-decoration-none gap-3 text-muted d-inline-flex w-100 align-items-center justify-content-start"
