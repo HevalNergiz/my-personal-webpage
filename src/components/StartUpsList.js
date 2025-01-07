74a8d9e23017f741056c5002d953d7fcf274497a
Heval Nergiz <1hevalnergiz1@gmail.com>
2025-01-07 01:00:12 +0100

    Add new entries to StartUpsList for Saga Legal and Izmir Atatürk High School Alumni Association

--- src/components/StartUpsList.js
+++ src/components/StartUpsList.js
@@ -5,6 +5,34 @@ const StartUpsList = () => {
     <div>
       <h3 className="mb-4 h5">I'm part of the...</h3>
       <ul className="mb-0 fw-light d-grid gap-3 partof-list ps-0 ps-md-4">
+        {/* Saga Legal and  Izmir Ataturk High School Alumni*/}
+        <li>
+          <a
+            className="text-decoration-none gap-3 text-muted d-inline-flex w-100 align-items-center justify-content-start"
+            href="https://www.sagalegal.io"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="">
+              <span className="border-bottom border-dark">Saga Legal</span>
+              <IconArrowUpRight className="ms-2" size={16}></IconArrowUpRight>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-decoration-none gap-3 text-muted d-inline-flex w-100 align-items-center justify-content-start"
+            href="https://www.ialmezun.org.tr"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="">
+              <span className="border-bottom border-dark">Izmir Atatürk High School Alumni Association</span>
+              <IconArrowUpRight className="ms-2" size={16}></IconArrowUpRight>
+            </span>
+          </a>
+        </li>
+
       
         {/* <li>
           <a
