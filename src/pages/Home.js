89044831d2b3e31d7f3c3bde866d54d4cd35595f
Heval Nergiz <1hevalnergiz1@gmail.com>
2025-03-23 20:29:35 +0100

    Refactor Home.js and update Portfolio.js iframe source URL

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,15 @@
 import React from "react";
-import Profile from "../components/Profile";
+import "../assets/css/styles.css";
 import About from "../components/About";
-import Technologies from "../components/Technologies";
-import StartUps from "../components/StartUps";
-import StartUpsList from "../components/StartUpsList";
+import ArticlesList from "../components/ArticlesList";
+import BgImage from "../components/BgImage";
 import Footer from "../components/Footer";
 import Menu from "../components/Menu";
+import Profile from "../components/Profile";
+import StartUps from "../components/StartUps";
+import StartUpsList from "../components/StartUpsList";
+import Technologies from "../components/Technologies";
 import Wrapper from "../components/Wrapper";
-import ArticlesList from "../components/ArticlesList";
-import "../assets/css/styles.css";
-import BgImage from "../components/BgImage";
-import ErrorTrigger from "../components/ErrorTrigger";
 
 function Home() {
   return (
@@ -24,7 +23,6 @@ function Home() {
         <ArticlesList></ArticlesList>
         <Footer></Footer>
         <Menu></Menu>
-        <ErrorTrigger></ErrorTrigger>
       </Wrapper>
       <BgImage alt="ephesus" width="100%" height="auto"></BgImage>
     </>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,7 +7,7 @@ function Portfolio() {
         title="portfolio file"
         className="vh-100"
         width="100%"
-        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FTbp7AfMnZWdJqPNNoYdPXd%2FHeval-Portfolio%3Fnode-id%3D0%253A1%26t%3Dy9OUz7deSIP29XW4-1"
+        src="https://embed.figma.com/design/nln69aUfX1DuXxnMWekCJC/SOME-WORKS?node-id=0-1&embed-host=share"
         allowfullscreen
       ></iframe>
       <Menu></Menu>
